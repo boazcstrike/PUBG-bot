f9eff19c5158335b770cb3ac9a74fc64de8b5db1
boazcstrike <boazcstrike@yahoo.com.sg>
2017-10-03 03:30:38 +0800

    Fixed user inputs and general output

--- bot.js
+++ bot.js
@@ -12,7 +12,9 @@ var hosts6 = ["52.64.63.253", "52.64.191.252", "52.65.63.252", "54.66.0.2", "54.
 var hosts7 = ["54.79.127.252", "54.153.191.252", "54.206.127.254", "54.206.128.2", "54.252.88.8"];
 var hosts8 = ["54.253.0.1"];
 
-var hosts = ["13.228.0.251", "46.51.216.14", "46.137.255.254", "52.74.0.2", "52.76.0.2", "52.76.191.252", "52.77.63.252", "52.221.255.252","54.151.128.2", "54.169.0.2", "54.169.191.253", "54.179.0.2", "54.179.191.252", "54.251.63.255","54.254.128.1", "54.255.0.2", "122.248.255.254", "175.41.146.190", "13.54.63.252", "52.62.63.252", "52.64.63.253", "52.64.191.252", "52.65.63.252", "54.66.0.2", "54.66.191.252", "54.79.127.252", "54.153.191.252", "54.206.127.254", "54.206.128.2", "54.252.88.8", "54.253.0.1"]
+var hosts = ["13.228.0.251", "46.51.216.14", "46.137.255.254", "52.74.0.2", "52.76.0.2", "52.76.191.252", "52.77.63.252", "52.221.255.252","54.151.128.2", "54.169.0.2", "54.169.191.253", "54.179.0.2", "54.179.191.252", "54.251.63.255","54.254.128.1", "54.255.0.2", "122.248.255.254", "175.41.146.190", "13.54.63.252", "52.62.63.252", "52.64.63.253", "52.64.191.252", "52.65.63.252", "54.66.0.2", "54.66.191.252", "54.79.127.252", "54.153.191.252", "54.206.127.254", "54.206.128.2", "54.252.88.8", "54.253.0.1"];
+
+var msglg = "";
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -46,6 +48,12 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: 'Pong!'
                 });
+            //!help
+            case 'help':
+                bot.sendMessage({
+                    to: channelID,
+                    message: '```pubg servers down? type .ping\ntype .1 until .4 to view Singapore servers\ntype .5 until .8 to view Australian Servers```'
+                });
             break;
             // Just add any case commands if you want to..
          }
@@ -59,63 +67,21 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
         //.ping
         if(cmd == 'ping'){
-
-            hosts1.forEach(function(host){
-                ping.sys.probe(host, function(isAlive){
-                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
-                    // console.log(msg);
-                    bot.sendMessage({
-                        to: channelID,
-                        message: msg
-                    });
-                });
-            });;
-
-            hosts2.forEach(function(host){
-                ping.sys.probe(host, function(isAlive){
-                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
-                    // console.log(msg);
-                    bot.sendMessage({
-                        to: channelID,
-                        message: msg
-                    });
-                });
-            });;
-
-            hosts3.forEach(function(host){
-                ping.sys.probe(host, function(isAlive){
-                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
-                    // console.log(msg);
-                    bot.sendMessage({
-                        to: channelID,
-                        message: msg
-                    });
-                });
-            });;
-
-            hosts4.forEach(function(host){
-                ping.sys.probe(host, function(isAlive){
-                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
-                    // console.log(msg);
-                    bot.sendMessage({
-                        to: channelID,
-                        message: msg
-                    });
-                });
-            });;
+            msglg += "```(Type .ping again if nothing shows up)";
 
             hosts.forEach(function(host){
                 ping.sys.probe(host, function(isAlive){
-                    var msg = isAlive ? '\nPUBG-SEA: ' + host + ' online' : '\nPUBG-SEA: ' + host + ' offline';
-                    console.log(msg);
-
-                    // bot.sendMessage({
-                    //     to: channelID,
-                    //     message: msg
-                    // });
-                
+                    msglg += isAlive ? '\n' + host + ': active' : '\n' + host + ' down';
                 });
-            });;
+            });
+
+            msglg += "```";
+            bot.sendMessage({
+                to: channelID,
+                message: msglg
+            });
+
+            msglg = "";
 
             // hosts.forEach(function (host) {
             //     ping.promise.probe(host)
@@ -127,8 +93,95 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             //             console.log(max);
             //         })
             //         .done();
-            // });
-          
+            // })
+        }
+        else if(cmd === '1'){
+            hosts1.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });
+        }
+        else if(cmd === '3'){
+            hosts1.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });
+        }
+        else if(cmd === '2'){
+            hosts1.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });
+        }
+        else if(cmd === '4'){
+            hosts1.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });
+        }
+        else if(cmd === '5'){
+            hosts1.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-AUS: ' + host + ' online' : '\nPUBG-AUS: ' + host + ' offline';
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });
+        }
+        else if(cmd === '6'){
+            hosts1.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-AUS: ' + host + ' online' : '\nPUBG-AUS: ' + host + ' offline';
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });
+        }
+        else if(cmd === '7'){
+            hosts1.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-AUS: ' + host + ' online' : '\nPUBG-AUS: ' + host + ' offline';
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });
+        }
+        else if(cmd === '8'){
+            hosts1.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-AUS: ' + host + ' online' : '\nPUBG-AUS: ' + host + ' offline';
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });
         }
      }
 
