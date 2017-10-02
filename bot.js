8995d58e567369426a89f96bc74d2e8408268a65
boazcstrike <boazcstrike@yahoo.com.sg>
2017-10-03 02:18:37 +0800

    added pinging to pubg southeast asian servers, discord bot taking the computer speed of output too much to handle, tried timeout but doesnt work with ping compatibility

---
+++ bot.js
@@ -0,0 +1,135 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+var ping = require('ping');
+
+var hosts1 = ["13.228.0.251", "46.51.216.14", "46.137.255.254","52.74.0.2", "52.76.0.2"];
+var hosts2 = ["52.76.191.252", "52.77.63.252", "52.221.255.252","54.151.128.2", "54.169.0.2"];
+var hosts3 = ["54.169.191.253", "54.179.0.2", "54.179.191.252", "54.251.63.255","54.254.128.1"];
+var hosts4 = ["54.255.0.2", "122.248.255.254", "175.41.146.190"];
+var hosts5 = ["13.54.63.252", "52.62.63.252", "52.64.63.253", "52.64.191.252", "52.65.63.252"];
+var hosts6 = ["52.64.63.253", "52.64.191.252", "52.65.63.252", "54.66.0.2", "54.66.191.252"];
+var hosts7 = ["54.79.127.252", "54.153.191.252", "54.206.127.254", "54.206.128.2", "54.252.88.8"];
+var hosts8 = ["54.253.0.1"];
+
+var hosts = ["13.228.0.251", "46.51.216.14", "46.137.255.254", "52.74.0.2", "52.76.0.2", "52.76.191.252", "52.77.63.252", "52.221.255.252","54.151.128.2", "54.169.0.2", "54.169.191.253", "54.179.0.2", "54.179.191.252", "54.251.63.255","54.254.128.1", "54.255.0.2", "122.248.255.254", "175.41.146.190", "13.54.63.252", "52.62.63.252", "52.64.63.253", "52.64.191.252", "52.65.63.252", "54.66.0.2", "54.66.191.252", "54.79.127.252", "54.153.191.252", "54.206.127.254", "54.206.128.2", "54.252.88.8", "54.253.0.1"]
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+    // console.log('PUBG-BOT' + ' - (' + bot.id + ') here! I am online!');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+
+     if (message.substring(0, 1) == '.') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+
+        //.ping
+        if(cmd == 'ping'){
+
+            hosts1.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
+                    // console.log(msg);
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });;
+
+            hosts2.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
+                    // console.log(msg);
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });;
+
+            hosts3.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
+                    // console.log(msg);
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });;
+
+            hosts4.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-SG: ' + host + ' online' : '\nPUBG-SG: ' + host + ' offline';
+                    // console.log(msg);
+                    bot.sendMessage({
+                        to: channelID,
+                        message: msg
+                    });
+                });
+            });;
+
+            hosts.forEach(function(host){
+                ping.sys.probe(host, function(isAlive){
+                    var msg = isAlive ? '\nPUBG-SEA: ' + host + ' online' : '\nPUBG-SEA: ' + host + ' offline';
+                    console.log(msg);
+
+                    // bot.sendMessage({
+                    //     to: channelID,
+                    //     message: msg
+                    // });
+                
+                });
+            });;
+
+            // hosts.forEach(function (host) {
+            //     ping.promise.probe(host)
+            //         .then(function (max) {
+            //             bot.sendMessage({
+            //                 to: channelID,
+            //                 message: max
+            //             });
+            //             console.log(max);
+            //         })
+            //         .done();
+            // });
+          
+        }
+     }
+
+});
